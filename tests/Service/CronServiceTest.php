fe9a7c6d6d2ed3b26ae14330bbdb8214277e0139
maxgu <maxgu@t4web.com.ua>
2016-02-27 13:01:12 +0200

    add CronService test

--- tests/Service/CronServiceTest.php
+++ tests/Service/CronServiceTest.php
@@ -2,8 +2,12 @@
 
 namespace T4web\CronTest\Service;
 
+use Symfony\Component\Process\Process;
 use Cron\Cron;
 use T4web\Cron\Service\CronService;
+use T4web\Cron\Exception\TimeoutException;
+use T4web\Cron\Executor\Executor;
+use T4web\Cron\Job\ShellJob;
 
 class CronServiceTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,6 +19,29 @@ class CronServiceTest extends \PHPUnit_Framework_TestCase
         $cron->isRunning()->willReturn(false);
 
         $service = new CronService(null, $cron->reveal());
+
+        $service->run();
+    }
+
+    public function testRunWithTimeout()
+    {
+        $cron = $this->prophesize(Cron::class);
+        $executor = $this->prophesize(Executor::class);
+        $job = $this->prophesize(ShellJob::class);
+        $process = $this->prophesize(Process::class);
+
+        $cron->run()->willReturn(null);
+        $cron->isRunning()->willReturn(false);
+
+        $service = new CronService(0.5, $cron->reveal());
+
+        $jobs = [$job->reveal()];
+        $process->getCommandLine()->willReturn('ls -la');
+        $job->getProcess()->willReturn($process->reveal());
+        $executor->getRunningJobs()->willReturn($jobs);
+        $cron->getExecutor()->willReturn($executor->reveal());
+
+        $this->setExpectedException(TimeoutException::class);
 
         $service->run();
     }
